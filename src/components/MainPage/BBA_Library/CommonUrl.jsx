21b4453500e84403de3887289dc89d806037b515
Ismayel Hossen <ismayelhossen123@gmail.com>
2023-01-08 14:16:18 +0600

    Report Generate Style changed

--- src/components/MainPage/BBA_Library/CommonUrl.jsx
+++ src/components/MainPage/BBA_Library/CommonUrl.jsx
@@ -1,3 +1,3 @@
-export const BaseUrl = "http://localhost:4123";
+export const BaseUrl = "http://localhost:4123/api";
 export const SelfUrl = "http://localhost:3000";
 export const SelfUrl3 = "http://localhost:3000";
